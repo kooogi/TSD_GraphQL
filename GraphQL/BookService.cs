7a0d08a8bd40915ef4308be2980214ee94bc0d43
kooogi <bartosz.chwilkowski00@gmail.com>
2024-06-03 19:31:39 +0200

    solution

---
+++ GraphQL/BookService.cs
@@ -0,0 +1,23 @@
+﻿namespace GraphQL
+{
+    public class BookService
+    {
+        private readonly List<Book> _books = new List<Book>
+    {
+        new Book { Id = 1, Title = "1984", Author = "George Orwell" },
+        new Book { Id = 2, Title = "The Hobbit", Author = "J.R.R. Tolkien" }
+    };
+
+        public List<Book> GetBooks() => _books;
+
+        public Book AddBook(int id, string title, string author)
+        {
+            var book = new Book { Id = id, Title = title, Author = author };
+            _books.Add(book);
+            return book;
+        }
+
+        public Book GetBookById(int id) => _books.FirstOrDefault(b => b.Id == id);
+    }
+
+}
--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using HotChocolate;
+using System.Collections.Generic;
+using GraphQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +12,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Register the BookService
+builder.Services.AddSingleton<BookService>();
+
 // Add GraphQL services
 builder.Services.AddGraphQLServer()
         .AddQueryType<Query>()
+        .AddMutationType<Mutation>()
         .AddApolloTracing();
 
 var app = builder.Build();
@@ -36,6 +42,12 @@ app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapGraphQL();
+
+    // Set up a default route to redirect to /graphql
+    endpoints.MapGet("/", async context =>
+    {
+        context.Response.Redirect("/graphql", permanent: false);
+    });
 });
 
 app.Run();
@@ -43,5 +55,42 @@ app.Run();
 // Define the Query type
 public class Query
 {
+    private readonly BookService _bookService;
+
+    public Query(BookService bookService)
+    {
+        _bookService = bookService;
+    }
+
     public string Hello() => "Hello, world!";
+
+    // Task 1
+    public List<Book> GetBooks() => _bookService.GetBooks();
+
+    // Task 3
+    public Book GetBookById(int id) => _bookService.GetBookById(id);
+}
+
+// Task 1: Basic Query and Schema Definition
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+}
+
+// Task 2: Mutation to Add a Book
+public class Mutation
+{
+    private readonly BookService _bookService;
+
+    public Mutation(BookService bookService)
+    {
+        _bookService = bookService;
+    }
+
+    public Book AddBook(int id, string title, string author)
+    {
+        return _bookService.AddBook(id, title, author);
+    }
 }
